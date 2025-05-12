b70bdd5de3c0290de743a59452f3004c10104a3b
jorge <jorgerollandiez@gmail.com>
2025-05-12 04:36:42 +0200

    Completed delete method

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -77,6 +77,7 @@ class CashCardController {
 
     @DeleteMapping("/{id}")
     private ResponseEntity<Void> deleteCashCard(@PathVariable Long id) {
+        cashCardRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
